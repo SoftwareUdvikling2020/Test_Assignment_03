45b2590fd2cb77f0f6b771bc42e3c0c4d4840e35
CONVERGENS\mak <mak@convergens.lan>
2020-10-25 15:09:36 +0100

    Booking test

--- src/test/java/integration/datalayer/customer/CreateBookingTest.java
+++ src/test/java/integration/datalayer/customer/CreateBookingTest.java
@@ -3,13 +3,19 @@ package integration.datalayer.customer;
 
 import datalayer.booking.BookingStorage;
 import datalayer.booking.BookingStorageImpl;
+import dto.BookingCreation;
 import org.flywaydb.core.Flyway;
 import org.flywaydb.core.api.configuration.FluentConfiguration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 
+import java.sql.Date;
 import java.sql.SQLException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @Tag("integration")
@@ -31,10 +37,30 @@ public class CreateBookingTest {
         flyway.migrate();
 
         bookingStorage = new BookingStorageImpl(url+db, "root", "testuser123");
+    }
 
-        var numCustomers = customerStorage.getCustomers().size();
-        if (numCustomers < 100) {
-            addFakeCustomers(100 - numCustomers);
-        }
+    @Test
+    public void mustSaveBookingInDatabaseWhenCallingCreateBooking() throws SQLException {
+        // Arrange
+        // Act
+        bookingStorage.createBooking(new BookingCreation(1, 1,null,null,null));
+        var booking = bookingStorage.getBookingsForCustomer(1);
+        // Assert
+        assertTrue(
+                booking.stream().anyMatch(x ->
+                        x.getEmployeeId() == 1 &&
+                                x.getCustomerId() == 1));
+    }
+
+    @Test
+    public void mustReturnLatestId() throws SQLException {
+        // Arrange
+        // Act
+        var bday = new Date(1239821l);
+        var id1 = bookingStorage.createBooking(new BookingCreation(2, 2,bday,null,null));
+        var id2 = bookingStorage.createBooking(new BookingCreation(1, 1,bday,null,null));
+
+        // Assert
+        assertEquals(1, id2 - id1);
     }
 }
